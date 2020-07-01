23963333b2a8cb7c92776e2c81775c2f52aa98f5
hay <hay.1020.tl@gmail.com>
2020-07-01 22:41:18 +0900

    final commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">登録</a>
                         @endif
                     @endauth
                 </div>
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ToDoList
                 </div>
 
                 <div class="links">
