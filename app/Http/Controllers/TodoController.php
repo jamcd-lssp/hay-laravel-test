ea3da42e8b1cc2cef8c38b999a18c2789c7b63bc
hay <hay.1020.tl@gmail.com>
2020-07-03 00:02:05 +0900

    change commit

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Todo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class TodoController extends Controller
 {
