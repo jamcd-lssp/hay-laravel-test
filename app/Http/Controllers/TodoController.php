0dae04b0c93a9aa36a274d24d2cb2bee20500a19
hay <hay.1020.tl@gmail.com>
2020-07-03 04:06:15 +0900

    change commit

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -12,8 +12,8 @@ class TodoController extends Controller
     {
     	if (Auth::check()) {
     		$user = Auth::user();
-    		$runningItems = Todo::with('user')->flg(1)->get();
-	    	$doneItems =  Todo::with('user')->flg(0)->get();
+    		$runningItems = Auth::user()->flg(1)->get();
+	    	$doneItems =  Auth::user()->flg(0)->get();
 	    	$param = [
 	    		'runningItems' => $runningItems,
 	    		'doneItems' => $doneItems,
