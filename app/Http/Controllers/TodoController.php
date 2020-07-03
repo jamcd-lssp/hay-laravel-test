e240cadfa80e2be0b24e9cf6fc1563c3ad8ff42b
hay <hay.1020.tl@gmail.com>
2020-07-03 19:11:08 +0900

    change commit

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -41,8 +41,8 @@ class TodoController extends Controller
 	public function update(Request $request)
 	{
 		$todo = $request->user()->todo();
-		$todo->flg = 0;
-		$todo->save($todo);
+		$reFlg = $todo->flg = 0;
+		$todo->fill($reFlg)->save();
 		return redirect('/todo');
 	}
 
