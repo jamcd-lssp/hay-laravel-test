ba6144e13cfcedaef3feda64ca743716b631fffc
hay <hay.1020.tl@gmail.com>
2020-07-04 23:12:23 +0900

    change commit

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,7 +16,7 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
         	'name' => 'test',
         	'email' => 'dummy@email.com',
-        	'password' => dcrypt('test1234'),
+        	'password' => bcrypt('test1234'),
         	'created_at' => Carbon::now(),
         	'updated_at' => Carbon::now(),
         ]);
