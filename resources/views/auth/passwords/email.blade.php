35fc71cc942b02cf670829cd7f82d9a14de1b1db
hay <hay.1020.tl@gmail.com>
2020-07-06 23:44:01 +0900

    change commit

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('パスワードリセット') }}</div>
+                <div class="card-header">{{ __('パスワード再発行') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -34,7 +34,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('パスワードリセット用のメールを受け取る') }}
+                                    {{ __('再発行リンクを送る') }}
                                 </button>
                             </div>
                         </div>
