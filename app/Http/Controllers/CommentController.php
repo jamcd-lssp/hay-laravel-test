a7370383b5210db98601a1832c29bdbc1c5bdba3
hay <hay.1020.tl@gmail.com>
2020-07-06 23:08:05 +0900

    change commit

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
