b6429affe6441db3f3d62268a0bc550efaf29161
hay <hay.1020.tl@gmail.com>
2020-07-06 16:03:53 +0900

    change commit

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -15,7 +15,7 @@ class TodoController extends Controller
     {
     	$folders = Auth::user()->todos()->get();
     	$current_folder = Todo::find($id);
-        $current_folder_title = Todo::where('title', $id)->get();
+        $current_folder_title = Todo::where('title', $current_folder)->get();
     	$tasks = $current_folder->tasks()->get();
     	return view('todo/index',[
     		'folders' => $folders,
