9b43b6a8c51a4bbd957292abe826019434c53cfe
hay <hay.1020.tl@gmail.com>
2020-07-07 11:45:15 +0900

    change commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-         Todo::class => FolderPolicy::class,
+        Todo::class => FolderPolicy::class,
     ];
 
     /**
