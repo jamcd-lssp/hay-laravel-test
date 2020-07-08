9d84509aac0e0603c97ad730b7a25c7ac8b4fa90
hay <hay.1020.tl@gmail.com>
2020-07-08 10:15:19 +0900

    change commit

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,13 @@ Route::group(['middleware' => 'auth'], function() {
 	Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 	Route::post('/folders/create', 'FolderController@create');
 
-	Route::group(['middleware' => 'can:view,todo'], function() {
-		Route::get('/folders/{id}/tasks', 'TodoController@index')->name('todo.index');
+	Route::get('/folders/{id}/tasks', 'TodoController@index')->name('todo.index');
 
-		Route::get('/folders/{id}/tasks/create', 'TodoController@showCreateForm')->name('todo.create');
-		Route::post('/folders/{id}/tasks/create', 'TodoController@create');
+	Route::get('/folders/{id}/tasks/create', 'TodoController@showCreateForm')->name('todo.create');
+	Route::post('/folders/{id}/tasks/create', 'TodoController@create');
 
-		Route::get('/folders/{id}/tasks/{task}/edit', 'TodoController@showEditForm')->name('todo.edit');
-		Route::post('/folders/{id}/tasks/{task}/edit', 'TodoController@edit');
-	});
+	Route::get('/folders/{id}/tasks/{task}/edit', 'TodoController@showEditForm')->name('todo.edit');
+	Route::post('/folders/{id}/tasks/{task}/edit', 'TodoController@edit');
 });
 
 Auth::routes();
